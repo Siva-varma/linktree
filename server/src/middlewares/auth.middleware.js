5e3efe103af1311f8cb34a012d13e5524d8aab3c
siva <sivavarma1999@gmail.com>
2026-06-20 12:20:50 +0530

    Add authentication middleware for token verification

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,16 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import apiError from "../utils/apiError.js";
+
+
+export const authMiddleware = asyncHandler(async (req, res, next) => {
+     let token = req.cookies.token;
+  if (!token) throw new apiError(401, "Token not found");
+
+  let user = await jwt.verify(token, process.env.JWT_SECRET);
+  if (!user) throw new apiError(401, "Unauthorized user");
+
+  req.user = user;
+
+  next();
+})
